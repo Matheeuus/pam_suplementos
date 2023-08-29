de999c684083a60f3d0d414a90df3e1ad5db8a21
Matheeuus <matheuspadiilha2012@hotmail.com>
2023-08-29 11:53:54 -0300

    FibraTec

---
+++ src/data/movies.js
@@ -0,0 +1,66 @@
+const DATA =
+[
+
+    {
+        id:'01',
+        nome:'Creatina Monohidratada 100g',
+        nota:'30R$',
+        imagem:'21.jpg'
+    },
+    {
+        id:'02',
+        nome:'Comprimidos growth',
+        nota:'70R$',
+        imagem:'22.jpg'
+   },
+   {
+        id:'03',
+        nome:'creatina growth 250g',
+        nota:'50R$',
+        imagem:'23.jpg'
+   },
+   {
+        id:'04',
+        nome:'creatina growth 100g',
+        nota:'25R$',
+        imagem:'24.jpg'
+   },
+   {
+        id:'05',
+        nome:'Creatina Monohidratada 250g',
+        nota:'60R$',
+        imagem:'25.jpg'
+   },
+   {
+        id:'06',
+        nome:'Multivitaminoco',
+        nota:'38R$',
+        imagem:'26.jpg' 
+   },
+   {
+        id:'07',
+        nome:'Colágeno varisol',
+        nota:'52R$',
+        imagem:'27.jpg' 
+   },
+   {
+    id:'08',
+    nome:'quitosana',
+    nota:'30R4',
+    imagem:'28.jpg' 
+   },
+   {
+    id:'09',
+    nome:'Multivitaminoco - Ultra',
+    nota:'50R$',
+    imagem:'29.jpg' 
+   },
+   {
+    id:'10',
+    nome:'picolinato',
+    nota:'10R$',
+    imagem:'30.jpg' 
+   },
+]
+
+export default DATA;
